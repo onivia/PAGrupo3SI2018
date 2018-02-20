e6626c36a81103c54ecf65921228bcdee184476d
onivia <ohnivia@gmail.com>
2018-02-19 20:39:03 -0500

    ejemplo.

--- 8pm_19feb2018_1/src/pruebas/Programa.java
+++ 8pm_19feb2018_1/src/pruebas/Programa.java
@@ -12,12 +12,17 @@ public class Programa {
     public static void main(String[] args) {
         int i = 2;
         
-        if(i == 10) {
-            System.out.println("i si es 10");                        
-        } else if(i == 5) {
-            System.out.println("i es igual a 5");
-        } else{
-            System.out.println("i ni es 5 ni es 10");            
+        switch (i) {
+            case 10:
+                System.out.println("i si es 10");
+                break;
+            case 2:
+            case 5:
+                System.out.println("i es igual a 5 o a 2");                
+                break;
+            default:
+                System.out.println("i ni es 5 ni es 10");
+                break;
         }
     }
 }
