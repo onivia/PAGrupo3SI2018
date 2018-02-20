a77d32b5d8ad7893d3644454e442f1edb7c893ea
onivia <ohnivia@gmail.com>
2018-02-19 20:32:05 -0500

    se creo el proyecto.

---
+++ 8pm_19feb2018_1/src/pruebas/Programa.java
@@ -0,0 +1,23 @@
+package pruebas;
+
+/**
+ *
+ * @author ONivia
+ */
+public class Programa {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int i = 2;
+        
+        if(i == 10) {
+            System.out.println("i si es 10");                        
+        } else if(i == 5) {
+            System.out.println("i es igual a 5");
+        } else{
+            System.out.println("i ni es 5 ni es 10");            
+        }
+    }
+}
