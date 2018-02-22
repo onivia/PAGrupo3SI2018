ab76ff5d66da90603af05b8aa1f1efa2aa484e42
onivia <ohnivia@gmail.com>
2018-02-21 20:35:10 -0500

    ejemplo.

--- 8pm_21feb2018_1/src/pruebas/Programa.java
+++ 8pm_21feb2018_1/src/pruebas/Programa.java
@@ -10,14 +10,23 @@ public class Programa {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        String mensaje1 = String.format("%1$-10.3s","hola","mundo");
-        String mensaje2 = String.format("%1$-10d", 100);
-        String mensaje3 = String.format("%1$-10.2f", 200.54964604860);
-        String mensaje4 = String.format("%1$e", 200.54964604860);
-        String mensaje5 = String.format("%1$g", 2.005496e+02);
-        String mensaje6 = String.format("%1$h", 10);
+        String nombre1 = "";
+        String nombre2 = "";
+        String nombre3 = "";
         
-        System.out.println(mensaje6);
+        String[] nombres = {"uno","tres","cinco"};
+        
+        for(String nombre : nombres) {
+            System.out.println(nombre);                       
+        }
+        
+        /*
+        int i = 0;
+        while(i < nombres.length) {
+            System.out.println(nombres[i]);
+
+            i++;
+        }
+        */
     }
-    
 }
