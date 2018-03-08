8e0eb9330c59b90a4a23b571a8340c1df68b1534
onivia <ohnivia@gmail.com>
2018-03-07 20:30:30 -0500

    Se crea proyecto C++

---
+++ 8pm_07mar2018_1/src/8pm_07mar2018_1.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : 8pm_07mar2018_1.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Hola Companeros!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
