cabb43d8a6bd94f69eeb9aa5c97b03802f02b2a6
onivia <ohnivia@gmail.com>
2018-03-21 20:36:41 -0500

    manejo de arreglos como si fueran apuntadores y apuntadores como si fueran arreglos.

--- 8pm_21mar2018_1/src/8pm_21mar2018_1.cpp
+++ 8pm_21mar2018_1/src/8pm_21mar2018_1.cpp
@@ -9,32 +9,18 @@
 #include <iostream>
 using namespace std;
 
-void f1(int **p1) {
-	int b = 20;
-
-	*p1 = &b;
-
-	cout<<**p1<<endl; //2°: 20
-}
-
-void f2(int *p1) {
-	int b = 20;
-
-	p1 = &b;
-
-	cout<<*p1<<endl; //2°: 20
+void f1(int *p1) {
+	cout<<*(p1 + 2)<<endl;
+	cout<<p1[2]<<endl;
 }
 
 int main() {
-	int a = 10;
+	int nums[] = {10,20,30};
 	int *p = NULL;
 
-	p = &a;
+	p = nums;
 
-	cout<<*p<<endl; //1°: 10
-	//f1(&p);
-	f2(p);
-	cout<<*p<<endl; //3°: ?
+	f1(p);
 
 	return 0;
 }
